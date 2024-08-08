f57c76a34a48575f06536d373ec2da88f8ed4287
0x5c0f <mail@0x5c0f.cc>
2024-08-08 16:59:14 +0800

    feat: 记录对话记录

--- relay/controller/helper.go
+++ relay/controller/helper.go
@@ -118,7 +118,27 @@ func postConsumeQuota(ctx context.Context, usage *relaymodel.Usage, meta *meta.M
 	if err != nil {
 		logger.Error(ctx, "error update user quota cache: "+err.Error())
 	}
-	logContent := fmt.Sprintf("模型倍率 %.2f，分组倍率 %.2f，补全倍率 %.2f", modelRatio, groupRatio, completionRatio)
+	
+	// logContent := fmt.Sprintf("模型倍率 %.2f，分组倍率 %.2f，补全倍率 %.2f", modelRatio, groupRatio, completionRatio)
+
+	var lastMsg string
+	if len(textRequest.Messages) > 0 {
+		if msg, ok := textRequest.Messages[len(textRequest.Messages)-1].Content.(string); ok {
+			lastMsg = msg
+			// 此处用于截断 fastgpt 带过来的其他内容
+			msgsplit := strings.Split(lastMsg, ">>>>")
+
+			if len(msgsplit) > 1 {
+				lastMsg = msgsplit[len(msgsplit)-1]
+			}
+
+		} else {
+			lastMsg = "unknown"
+		}
+	}
+
+	logContent := fmt.Sprintf("模型倍率 %.2f，分组倍率 %.2f，补全倍率 %.2f, Input: %s", modelRatio, groupRatio, completionRatio, lastMsg)
+
 	model.RecordConsumeLog(ctx, meta.UserId, meta.ChannelId, promptTokens, completionTokens, textRequest.Model, meta.TokenName, quota, logContent)
 	model.UpdateUserUsedQuotaAndRequestCount(meta.UserId, quota)
 	model.UpdateChannelUsedQuota(meta.ChannelId, quota)
